50212c5fca40016341df9d95bdf9ccc3e364bd1e
thanhthai_nguyen <17521031@gm.uit.edu.vn>
2020-04-24 00:18:19 +0700

    auth api

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const Token = require('../models/token');
